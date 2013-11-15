79f05840679a5ab2651af56e4161a5bc84af2c29
Joshua Blakely <Home@Joshua-Blakelys-iMac-2.local>
2013-11-15 14:02:02 -0600

    Initial Commit

---
+++ InitialTestGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  InitialTestGame
+//
+
+//  Copyright (c) 2013 Robotic Elephant. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ InitialTestGame/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  InitialTestGame
+//
+//  Created by Joshua Blakely on 11/15/13.
+//  Copyright (c) 2013 Robotic Elephant. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
